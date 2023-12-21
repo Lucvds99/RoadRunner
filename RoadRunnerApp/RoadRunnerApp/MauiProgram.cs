0710f2c3a6031d6f88f95c31020d9b36ab9efe5d
lucas076k <lwj.kuijsters@student.avans.nl>
2023-12-21 10:27:08 +0100

    Manifest fixed and popup works properly

--- RoadRunnerApp/RoadRunnerApp/MauiProgram.cs
+++ RoadRunnerApp/RoadRunnerApp/MauiProgram.cs
@@ -16,11 +16,6 @@ namespace RoadRunnerApp
                     fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
                     fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
                 });
-                        .ConfigureImageSources((context, sources) =>
-                         {
-                             // Voeg het pad naar je app-pictogram toe
-                             sources.Add("appicon", "Resources/your_app_icon.png");
-                         });
 
 #if DEBUG
             builder.Logging.AddDebug();
