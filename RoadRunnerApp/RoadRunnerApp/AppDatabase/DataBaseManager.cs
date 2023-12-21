8a598c14bd06bfc6397c6e516faae03eab5fc771
Thom de Wit <tj.dewit2@student.avans.nl>
2023-12-21 13:12:11 +0100

    Added all sights

--- RoadRunnerApp/RoadRunnerApp/AppDatabase/DataBaseManager.cs
+++ RoadRunnerApp/RoadRunnerApp/AppDatabase/DataBaseManager.cs
@@ -92,32 +92,409 @@ class DatabaseManager
         sights.Add(new Sight
         {
             Id = 1,
-            Longitude = 4.77942,
-            Latitude = 51.59411,
+            Longitude = 4.7794166667,
+            Latitude = 51.5941116667,
             Name = "Oude VVV-pand",
             Description = "Het Oude pand van de VVV",
-            Category = Categories.Museum,
-            ImgFilePath = "/path/to/image1.jpg"
+            ImgFilePath = "1"
         });
         sights.Add(new Sight
         {
             Id = 2,
-            Longitude = 4.77939,
-            Latitude = 51.59328,
+            Longitude = 4.7793883333,
+            Latitude = 51.5932783333,
             Name = "Liefdeszuster",
-            Description = "Leifdeszusters pant",
-            Category = Categories.Park,
-            ImgFilePath = "/path/to/image2.jpg"
+            Description = "",
+            ImgFilePath = "3"
         });
         sights.Add(new Sight
         {
             Id = 3,
-            Longitude = 4.77970,
-            Latitude = 51.57583,
+            Longitude = 4.779695,
+            Latitude = 51.5925,
             Name = "Nassau Baronie Monument",
             Description = "",
             Category = Categories.Monument,
-            ImgFilePath = "/path/to/image3.jpg"
+            ImgFilePath = "4"
+        });
+        sights.Add(new Sight
+        {
+            Id = 4,
+            Longitude = 4.7793883333,
+            Latitude = 51.5925,
+            Name = "",
+            Description = "",
+            ImgFilePath = ""
+        });
+        sights.Add(new Sight
+        {
+            Id = 5,
+            Longitude = 4.7784716667,
+            Latitude = 51.5928333333,
+            Name = "The Light House",
+            Description = "",
+            ImgFilePath = "5"
+        });
+        sights.Add(new Sight
+        {
+            Id = 6,
+            Longitude = 4.7779166667,
+            Latitude = 51.5926666667,
+            Name = "",
+            Description = "",
+            ImgFilePath = ""
+        });
+        sights.Add(new Sight
+        {
+            Id = 7,
+            Longitude = 4.777,
+            Latitude = 51.5906116667,
+            Name = "",
+            Description = "",
+            ImgFilePath = "13"
+        });
+        sights.Add(new Sight
+        {
+            Id = 8,
+            Longitude = 4.7761666667,
+            Latitude = 51.5906116667,
+            Name = "Kasteel van Breda",
+            Description = "",
+            ImgFilePath = "9"
+        });
+        sights.Add(new Sight
+        {
+            Id = 9,
+            Longitude = 4.7761383333,
+            Latitude = 51.589695,
+            Name = "Stadhouderspoort",
+            Description = "",
+            ImgFilePath = "11"
+        });
+        sights.Add(new Sight
+        {
+            Id = 10,
+            Longitude = 4.776,
+            Latitude = 51.5903333333,
+            Name = "",
+            Description = "",
+            ImgFilePath = "8"
+        });
+        sights.Add(new Sight
+        {
+            Id = 11,
+            Longitude = 4.775,
+            Latitude = 51.5903883333,
+            Name = "",
+            Description = "",
+            ImgFilePath = "7"
+        });
+        sights.Add(new Sight
+        {
+            Id = 12,
+            Longitude = 4.7743616667,
+            Latitude = 51.5900283333,
+            Name = "Huis van Brecht",
+            Description = "",
+            ImgFilePath = "6"
+        });
+        sights.Add(new Sight
+        {
+            Id = 13,
+            Longitude = 4.773445,
+            Latitude = 51.590195,
+            Name = "Spanjaardsgat",
+            Description = "",
+            ImgFilePath = "20"
+        });
+        sights.Add(new Sight
+        {
+            Id = 14,
+            Longitude = 4.7733333333,
+            Latitude = 51.5898333333,
+            Name = "Begin Vismarkt",
+            Description = "",
+            ImgFilePath = "19"
+        });
+        sights.Add(new Sight
+        {
+            Id = 15,
+            Longitude = 4.774445,
+            Latitude = 51.5893616667,
+            Name = "Begin Havermarkt",
+            Description = "",
+            ImgFilePath = ""
+        });
+        sights.Add(new Sight
+        {
+            Id = 16,
+            Longitude = 4.7798883333,
+            Latitude = 51.5887783333,
+            Name = "",
+            Description = "",
+            ImgFilePath = "10"
+        });
+        sights.Add(new Sight
+        {
+            Id = 17,
+            Longitude = 4.7802783333,
+            Latitude = 51.5888333333,
+            Name = "Grote Kerk",
+            Description = "",
+            ImgFilePath = "17"
+        });
+        sights.Add(new Sight
+        {
+            Id = 18,
+            Longitude = 4.7748883333,
+            Latitude = 51.5887783333,
+            Name = "",
+            Description = "",
+            ImgFilePath = "15"
+        });
+        sights.Add(new Sight
+        {
+            Id = 19,
+            Longitude = 4.7751383333,
+            Latitude = 51.588195,
+            Name = "Het Poortje",
+            Description = "",
+            ImgFilePath = ""
+        });
+        sights.Add(new Sight
+        {
+            Id = 20,
+            Longitude = 4.77575,
+            Latitude = 51.5870833333,
+            Name = "Ridderstraat",
+            Description = "",
+            ImgFilePath = "17"
+        });
+        sights.Add(new Sight
+        {
+            Id = 21,
+            Longitude = 4.776555,
+            Latitude = 51.5874166667,
+            Name = "Grote Markt",
+            Description = "",
+            ImgFilePath = "24"
+        });
+        sights.Add(new Sight
+        {
+            Id = 22,
+            Longitude = 4.7763333333,
+            Latitude = 51.5880283333,
+            Name = "Bevrijdingsmonument",
+            Description = "",
+            Category = Categories.Monument,
+            ImgFilePath = ""
+        });
+        sights.Add(new Sight
+        {
+            Id = 23,
+            Longitude = 4.7761116667,
+            Latitude = 51.58875,
+            Name = "Stadhuis",
+            Description = "",
+            ImgFilePath = ""
+        });
+        sights.Add(new Sight
+        {
+            Id = 24,
+            Longitude = 4.7763616667,
+            Latitude = 51.5879716667,
+            Name = "",
+            Description = "",
+            ImgFilePath = ""
+        });
+        sights.Add(new Sight
+        {
+            Id = 25,
+            Longitude = 4.776555,
+            Latitude = 51.5875,
+            Name = "",
+            Description = "",
+            ImgFilePath = ""
+        });
+        sights.Add(new Sight
+        {
+            Id = 26,
+            Longitude = 4.77725,
+            Latitude = 51.5876383333,
+            Name = "Antonius van Paduakerk",
+            Description = "",
+            ImgFilePath = ""
+        });
+        sights.Add(new Sight
+        {
+            Id = 27,
+            Longitude = 4.7785,
+            Latitude = 51.5882783333,
+            Name = "",
+            Description = "",
+            ImgFilePath = ""
+        });
+        sights.Add(new Sight
+        {
+            Id = 28,
+            Longitude = 4.778945,
+            Latitude = 51.588,
+            Name = "Bibliotheek",
+            Description = "",
+            ImgFilePath = ""
+        });
+        sights.Add(new Sight
+        {
+            Id = 29,
+            Longitude = 4.7802216667,
+            Latitude = 51.5873616667,
+            Name = "",
+            Description = "",
+            ImgFilePath = ""
+        });
+        sights.Add(new Sight
+        {
+            Id = 30,
+            Longitude = 4.7810283333,
+            Latitude = 51.5877216667,
+            Name = "Kloosterkazerne",
+            Description = "",
+            ImgFilePath = ""
+        });
+        sights.Add(new Sight
+        {
+            Id = 31,
+            Longitude = 4.782,
+            Latitude = 51.58775,
+            Name = "Chasse theater",
+            Description = "",
+            ImgFilePath = ""
+        });
+        sights.Add(new Sight
+        {
+            Id = 32,
+            Longitude = 4.78125,
+            Latitude = 51.58775,
+            Name = "",
+            Description = "",
+            ImgFilePath = ""
+        });
+        sights.Add(new Sight
+        {
+            Id = 33,
+            Longitude = 4.7808883333,
+            Latitude = 51.5886116667,
+            Name = "Binding van Isaac",
+            Description = "",
+            ImgFilePath = ""
+        });
+        sights.Add(new Sight
+        {
+            Id = 34,
+            Longitude = 4.780445,
+            Latitude = 51.5895,
+            Name = "",
+            Description = "",
+            ImgFilePath = ""
+        });
+        sights.Add(new Sight
+        {
+            Id = 35,
+            Longitude = 4.781,
+            Latitude = 51.5896666667,
+            Name = "Beyerd",
+            Description = "",
+            ImgFilePath = ""
+        });
+        sights.Add(new Sight
+        {
+            Id = 36,
+            Longitude = 4.780445,
+            Latitude = 51.5895,
+            Name = "",
+            Description = "",
+            ImgFilePath = ""
+        });
+        sights.Add(new Sight
+        {
+            Id = 37,
+            Longitude = 4.78,
+            Latitude = 51.589555,
+            Name = "Gasthuispoort",
+            Description = "",
+            ImgFilePath = ""
+        });
+        sights.Add(new Sight
+        {
+            Id = 38,
+            Longitude = 4.7798616667,
+            Latitude = 51.5894166667,
+            Name = "",
+            Description = "",
+            ImgFilePath = ""
+        });
+        sights.Add(new Sight
+        {
+            Id = 39,
+            Longitude = 4.779695,
+            Latitude = 51.5890283333,
+            Name = "",
+            Description = "",
+            ImgFilePath = ""
+        });
+        sights.Add(new Sight
+        {
+            Id = 40,
+            Longitude = 4.7783333333,
+            Latitude = 51.588555,
+            Name = "",
+            Description = "",
+            ImgFilePath = ""
+        });
+        sights.Add(new Sight
+        {
+            Id = 41,
+            Longitude = 4.777945,
+            Latitude = 51.5891116667,
+            Name = "Willem Merkxtuin",
+            Description = "",
+            ImgFilePath = ""
+        });
+        sights.Add(new Sight
+        {
+            Id = 42,
+            Longitude = 4.777805,
+            Latitude = 51.5896666667,
+            Name = "",
+            Description = "",
+            ImgFilePath = ""
+        });
+        sights.Add(new Sight
+        {
+            Id = 43,
+            Longitude = 4.7783616667,
+            Latitude = 51.589695,
+            Name = "Begijnenhof",
+            Description = "",
+            ImgFilePath = ""
+        });
+        sights.Add(new Sight
+        {
+            Id = 44,
+            Longitude = 4.777805,
+            Latitude = 51.5896666667,
+            Name = "",
+            Description = "",
+            ImgFilePath = ""
+        });
+        sights.Add(new Sight
+        {
+            Id = 45,
+            Longitude = 4.77625,
+            Latitude = 51.5895,
+            Name = "Einde stadswandeling",
+            Description = "",
+            ImgFilePath = ""
         });
 
         this.sqlite_conn.InsertAll(sights);
