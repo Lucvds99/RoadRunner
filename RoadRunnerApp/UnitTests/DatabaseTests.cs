6ce96dd7d735e0942bd195cacb4b65a3381f3937
Double <Lucvds2001@Gmail.com>
2024-01-05 15:16:31 +0100

    Structure update, structure for MapPage testing added

--- RoadRunnerApp/UnitTests/DatabaseTests.cs
+++ RoadRunnerApp/UnitTests/DatabaseTests.cs
@@ -1,8 +1,6 @@
 using RoadRunnerApp.AppDatabase;
-using RoadRunnerApp.AppRoutes;
-using System.Data.Common;
-using Mlocation = Microsoft.Maui.Devices.Sensors.Location;
 using DBRoute = RoadRunnerApp.AppDatabase.Route;
+
 
 namespace UnitTests
 {
@@ -52,29 +50,6 @@ namespace UnitTests
 
             // Check if amount of retrieved sights out DB are the correct amount for a certain route
             Assert.Equal(3, amountOfSights);
-        }
-
-    }
-
-
-    public class RouteTests()
-    {
-
-
-        RouteManager routeMngr = new RouteManager();
-
-
-        [Fact]
-        public async void CheckDecodedRoute()
-        {
-            Mlocation userLocation = new Mlocation(51.58775, 4.782); // Test origin
-            Landmark testDest = new Landmark(1, "VVV-pand", "test description", "Unit Testing Theme", new CustomLocation(51.5941117, 4.7794167));
-            List<Mlocation> coordinatesList = await routeMngr.GetDecodedRoute(new List<Landmark> { testDest }, userLocation);
-
-            int amountOfCoordinates = coordinatesList.Count;
-            Assert.Equal(24, amountOfCoordinates);
-            
-
         }
 
     }
---
+++ RoadRunnerApp/UnitTests/MapPageTest.cs
@@ -0,0 +1,33 @@
+﻿using RoadRunnerApp.Views;
+
+namespace UnitTests
+{
+    public class MapPageTest()
+    {
+        MapPage mapPage = new MapPage();
+        [Fact]
+        public void CheckClosestLandmark()
+        {
+            //mapPage.GetClosestLandmark();
+
+            Assert.Equal(true, false);
+        }
+
+        [Fact]
+        public void CheckIsInDistance()
+        {
+            //mapPage.isInDistance();
+
+            Assert.Equal(true, false);
+        }
+
+        [Fact]
+        public void CheckLandMarksDistance()
+        {
+            //mapPage.GetClosestLandmark();
+
+            Assert.Equal(true, false);
+        }
+
+    }
+}
