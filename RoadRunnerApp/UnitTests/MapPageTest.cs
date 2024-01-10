1f237edce255473c1a45fea40d952207857857bf
Timo Vleer <t.vleer1@student.avans.nl>
2024-01-10 16:18:01 +0100

    2 vd 3 unit tests klaar

--- RoadRunnerApp/UnitTests/MapPageTest.cs
+++ RoadRunnerApp/UnitTests/MapPageTest.cs
@@ -1,24 +1,45 @@
-﻿using RoadRunnerApp.Views;
+﻿using RoadRunnerApp.AppRoutes;
+using RoadRunnerApp.Views;
 
 namespace UnitTests
 {
     public class MapPageTest()
     {
         MapPage mapPage = new MapPage();
-        [Fact]
-        public void CheckClosestLandmark()
-        {
-            //mapPage.GetClosestLandmark();
 
-            Assert.Equal(true, false);
+        [Theory]
+        [InlineData(10)]
+        public async void CheckClosestLandmark(double closestDistance)
+        {
+            // Arrange
+            List<Tuple<Landmark, double>> distances = new List<Tuple<Landmark, double>>
+        {
+            new Tuple<Landmark, double>(new Landmark(1, "Landmark1", "Description1", "Theme1", new CustomLocation(1.0, 2.0)), 10),
+            new Tuple<Landmark, double>(new Landmark(2, "Landmark2", "Description2", "Theme2", new CustomLocation(3.0, 4.0)), 20),
+        };
+
+            // Act
+            var closestTuple = await mapPage.GetClosestLandmark(distances);
+            var closestLandmark = closestTuple.Item1;
+
+            // Assert
+            Assert.NotNull(closestTuple);
+            Assert.Equal(closestDistance, closestTuple.Item2);
         }
 
-        [Fact]
-        public void CheckIsInDistance()
+        [Theory]
+        [InlineData(10.0, true)]
+        [InlineData(20.0, false)]
+        public void CheckIsInDistance(double distance, bool expectedResult)
         {
-            //mapPage.isInDistance();
+            // Arrange
+            var landmark = new Landmark(1, "Landmark1", "Description1", "Theme1", new CustomLocation(1.0, 2.0));
 
-            Assert.Equal(true, false);
+            // Act
+            bool isWithinThreshold = mapPage.isInDistance(Tuple.Create(landmark, distance));
+
+            // Assert
+            Assert.Equal(expectedResult, isWithinThreshold);
         }
 
         [Fact]
@@ -26,7 +47,7 @@ namespace UnitTests
         {
             //mapPage.GetClosestLandmark();
 
-            Assert.Equal(true, false);
+            
         }
 
     }
